14723040de1af6bd574b4645de5d36d4c369e82f
andresgrossov <andresgrosso@seven4n.com>
2016-04-17 18:34:16 -0500

    Se crean las 5 páginas para las manifestaciones y documental completo, así como la página para la infografía.

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,9 +235,40 @@
 
 						});
 
+						// // Find all YouTube videos
+						// var $allVideos = $("iframe[src^='http://www.youtube.com']"),
+						//
+						//     // The element that is fluid width
+						//     $fluidEl = $("body");
+						//
+						// // Figure out and save aspect ratio for each video
+						// $allVideos.each(function() {
+						//
+						//   $(this)
+						//     .data('aspectRatio', this.height / this.width)
+						//
+						//     // and remove the hard coded width/height
+						//     .removeAttr('height')
+						//     .removeAttr('width');
+						//
+						// });
+
 						$window.resize(function() {
 							reelWidth = $reel[0].scrollWidth;
 							$t._update();
+
+							// var newWidth = $fluidEl.width();
+							//
+							// // Resize all videos according to their own aspect ratio
+							// $allVideos.each(function() {
+							//
+							// 	var $el = $(this);
+							// 	$el
+							// 		.width(newWidth)
+							// 		.height(newWidth * $el.data('aspectRatio'));
+							//
+							// });
+
 						}).trigger('resize');
 
 					});
@@ -247,3 +278,21 @@
 	});
 
 })(jQuery);
+
+// // When the window is resized
+// $(window).resize(function() {
+//
+//   var newWidth = $fluidEl.width();
+//
+//   // Resize all videos according to their own aspect ratio
+//   $allVideos.each(function() {
+//
+//     var $el = $(this);
+//     $el
+//       .width(newWidth)
+//       .height(newWidth * $el.data('aspectRatio'));
+//
+//   });
+//
+// // Kick off one resize to fix all videos on page load
+// }).resize();
